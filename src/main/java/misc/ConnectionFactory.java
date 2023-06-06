e1d30a18f6f4dc6bc19599be9d6e187c3347467f
raphaelritz <r.ritz@student.utwente.nl>
2023-06-06 18:30:47 +0200

    Add option to pick between databases into connection factory

--- src/main/java/misc/ConnectionFactory.java
+++ src/main/java/misc/ConnectionFactory.java
@@ -8,40 +8,34 @@ import java.sql.SQLException;
  * Factory Pattern
  */
 public class ConnectionFactory {
-    private static final String HOST = "node29422-shotmaniacs1.paas.hosted-by-previder.com";
-    private static final String DB_NAME = "postgres";
-    private static final String URL = "jdbc:postgresql://" + HOST + ":5432/" + DB_NAME + "?currentSchema=shotmaniacs";
-    private static final String USERNAME = "webadmin";
-
-
-//    private static final String FILE_NAME = "di-2023-project-password";
+    private static final int DB = 1; // 1 = Bronto, 2 = Previder
+    private static final String PREVIDER_HOST = "node29422-shotmaniacs1.paas.hosted-by-previder.com";
+    private static final String BRONTO_HOST = "bronto.ewi.utwente.nl";
+    private static final String PREVIDER_DB_NAME = "postgres";
+    private static final String BRONTO_DB_NAME = "dab_di22232b_249";
+    private static final String PREVIDER_URL = "jdbc:postgresql://" + PREVIDER_HOST + ":5432/" + PREVIDER_DB_NAME + "?currentSchema=shotmaniacs";
+    private static final String BRONTO_URL = "jdbc:postgresql://" + BRONTO_HOST + ":5432/" + BRONTO_DB_NAME + "?currentSchema=shotmaniacs1";
+    private static final String PREVIDER_USERNAME = "webadmin";
+    private static final String BRONTO_USERNAME = "dab_di22232b_249";
     private static Connection connection;
     private static boolean connected = false;
-
-    public static void setup() {
-        try {
-            Class.forName("org.postgresql.Driver");
-            String password = readPassword();
-
-            System.out.println(password);
-            connection = DriverManager.getConnection(URL, USERNAME, password);
-            connected = true;
-            System.out.println("Connection successfully setup.");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.err.println("Driver is not loaded.");
-        } catch (SQLException e) {
-            System.err.println("Oops: "+e.getMessage());
-        }
-    }
+    //    private static final String FILE_NAME = "di-2023-project-password";
 
     public static Connection getConnection() {
         if (!connected) setup();
         return connection;
     }
 
-    private static String readPassword() {
-        return "EFPsql77037";
+    public static void setup() {
+        if (DB == 1) brontoFactory();
+        else if (DB == 2) previderFactory();
+        else System.err.println("Error: no valid database selected.");
+    }
+
+    private static String readPassword(int db) {
+        if (db == 1) return "Y8AHMmfXpf9glCor";
+        if (db == 2) return "EFPsql77037";
+        else return null;
 //        try {
 //            String path = System.getProperty("user.dir")+"/";
 //            var br = new BufferedReader(new FileReader(path+FILE_NAME));
@@ -54,5 +48,37 @@ public class ConnectionFactory {
 //            System.err.println("Error reading document: "+e.getMessage());
 //            return "";
 //        }
+    }
+
+    public static void brontoFactory() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String password = readPassword(1);
+            System.out.println(password);
+            connection = DriverManager.getConnection(BRONTO_URL, BRONTO_USERNAME, password);
+            connected = true;
+            System.out.println("Connection successfully setup.");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Driver is not loaded.");
+        } catch (SQLException e) {
+            System.err.println("Oops: " + e.getMessage());
+        }
+    }
+
+    public static void previderFactory() {
+        try {
+            Class.forName("org.postgresql.Driver");
+            String password = readPassword(2);
+            System.out.println(password);
+            connection = DriverManager.getConnection(PREVIDER_URL, PREVIDER_USERNAME, password);
+            connected = true;
+            System.out.println("Connection successfully setup.");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.err.println("Driver is not loaded.");
+        } catch (SQLException e) {
+            System.err.println("Oops: " + e.getMessage());
+        }
     }
 }
