3afb3b72be221a89c1583a3e2c9b0316c0e6df3d
Pavan-awadh <pawadhpersad@gmail.com>
2023-06-08 11:13:58 +0200

    Javascript admin announcement added

--- src/main/java/resources/AnnouncementResource.java
+++ src/main/java/resources/AnnouncementResource.java
@@ -1,5 +1,6 @@
 package resources;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import models.AnnouncementBean;
@@ -7,7 +8,9 @@ import models.AnnouncementBean;
 @Path("/admin/dashboard")
 public class AnnouncementResource {
 
+    @Path("/new")
     @POST
+    @Consumes
     public void handlePostAnnouncement(AnnouncementBean announcement) {
 
     }
