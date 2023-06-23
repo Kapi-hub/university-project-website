7f83b62b2a9523b2e12db5479bffdad243776360
raphaelritz <r.ritz@student.utwente.nl>
2023-06-23 13:58:08 +0200

    BugFix

--- src/main/java/models/CrewMemberBean.java
+++ src/main/java/models/CrewMemberBean.java
@@ -8,7 +8,7 @@ public class CrewMemberBean extends AccountBean {
     private Team team;
 
     public CrewMemberBean(int id){
-        this.id = id;
+        super(id);
     }
     public CrewMemberBean() {
         super();
