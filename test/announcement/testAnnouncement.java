692442a7485fba563b24378dc8584222d4f9589e
Baris <b.yilmaz-1@student.utwente.nl>
2023-06-27 17:41:10 +0200

    testing branch

--- test/announcement/testAnnouncement.java
+++ test/announcement/testAnnouncement.java
@@ -1,5 +1,6 @@
 package announcement;
 
+import jakarta.ws.rs.core.Response;
 import models.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,19 +12,22 @@ import java.sql.Timestamp;
 
 public class testAnnouncement {
 
-    AnnouncementBean announcementBean;
-    AnnouncementResponseBean announcementResponseBean;
-
-
-    @BeforeEach
-    void setup() {
-        announcementBean = new AnnouncementBean(1, 2,  "title",  "body", new Timestamp(1,1,1,1,1,1,1));
-        announcementResponseBean = new AnnouncementResponseBean();
-    }
+//    AnnouncementBean announcementBean;
+//    AnnouncementResponseBean announcementResponseBean;
+//    @BeforeEach
+//    void setup() {
+//        announcementBean = new AnnouncementBean(1, 2,  "title",  "body", new Timestamp(1,1,1,1,1,1,1));
+//        announcementResponseBean = new AnnouncementResponseBean();
+//    }
 
     @Test
     void testGetAnnouncementsForCrew() {
         AnnouncementResource resource = new AnnouncementResource();
-        Assertions.assertEquals(, resource.getAnnouncementsForCrew(announcementResponseBean.getI));
+        Assertions.assertEquals(Response.status(Response.Status.INTERNAL_SERVER_ERROR).build(), resource.getAnnouncementsForCrew("1")); //needs String, what is argument?
+        Assertions.assertEquals(Response.status(Response.Status.INTERNAL_SERVER_ERROR).build(), resource.getAnnouncementsForCrew("2")); //needs String, what is argument?
+        //nonexistent account
+        Assertions.assertEquals(Response.status(Response.Status.INTERNAL_SERVER_ERROR).build(), resource.getAnnouncementsForCrew("-4")); //needs String, what is argument?
+
+
     }
 }
