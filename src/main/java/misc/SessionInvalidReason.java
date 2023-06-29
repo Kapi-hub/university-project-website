3e75ea00df5e8c91bc51f3dd3571038965e04413
s3011313 <r.ritz@student.utwente.nl>
2023-06-29 11:55:02 +0200

    Remove all

--- src/main/java/misc/SessionInvalidReason.java
+++
@@ -1,7 +0,0 @@
-package misc;
-
-public enum SessionInvalidReason {
-    EXPIRED,
-    UNAUTHORIZED,
-    NOT_LOGGED_IN
-}
--- src/main/webapp/WEB-INF/publicResources/crew/calendar/monthScript.js
+++
@@ -1,4 +0,0 @@
-let li_items = document.querySelectorAll(".left_side_bar ul li");
-
-//highlight current month on page load
-li_items.item(new Date().getMonth()).classList.add("active");
